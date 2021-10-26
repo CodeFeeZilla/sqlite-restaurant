a47ee5ef740fa4144c8b9b1126ea31fe293cdbad
Abdi Arr <abdirizaqarr@gmail.com>
2021-10-26 14:52:45 +0100

    added joins

--- db.sql
+++ db.sql
@@ -1,3 +1,6 @@
+.headers on
+PRAGMA foreign_keys = on;
+
 -- Create Tables --
 CREATE TABLE Restaurants (
   restaurant_id INTEGER NOT NULL PRIMARY KEY,
@@ -24,51 +27,45 @@ CREATE TABLE MenuItems (
 -- Insert Entries (rows) into
   -- Restaurants
 INSERT INTO Restaurants (name, imagelink) VALUES ("SpagBrillo", "image1.jpg");
+INSERT INTO Restaurants (name, imagelink) VALUES ("FoodMaven", "genericimage.jpg");
 
   -- Menus
 INSERT INTO Menus (title, restaurant_id) 
 VALUES ("Super Saver", 1);
+INSERT INTO Menus (title, restaurant_id) 
+VALUES ("Maven Menu", 2);
 
   -- MenuItems
 INSERT INTO MenuItems (name, price, menu_id)
 VALUES ("Plain Spaghetti", 1, 1);
 INSERT INTO MenuItems (name, price, menu_id)
 VALUES ("Plain Pasta", 3, 1);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Flour, just flour", 43, 2);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Sweet Sugar", 56, 2);
 -- End Of Insert --
 
-SELECT name FROM sqlite_master WHERE type='table';
--- Read Back data from
-  -- Restaurants
-SELECT * from Restaurants;
+-- Joins --
+-- SELECT the name of a menu from a specific restaurant and list all the menu items on this menu
 
-  --Menus
-SELECT * from Menus;
-
-  --MenuItems
-SELECT * from MenuItems;
--- End of Read --
-
--- UPDATE rows of
-  -- Restaurants
-UPDATE Restaurants SET restaurant_id=2 where restaurant_id=1;
-  --Menus
-UPDATE Menus SET title="Mister Menu" WHERE title="Super Saver";
-  --MenuItems
-UPDATE MenuItems SET price=1000 where item_id=1;
+SELECT title, name FROM Menus
+JOIN MenuItems ON
+Menus.id = MenuItems.menu_id
+WHERE title = "Super Saver";
 
 
--- Read Back data after updates from
-  -- Restaurants
-SELECT * from Restaurants;
+-- --SELECT all the restaurants with a count of the number of menus each restaurant has. Hint: you will need to use the COUNT and GROUP BY keywords.
 
-  --Menus
-SELECT * from Menus;
+SELECT name, COUNT(title) FROM
+Restaurants JOIN
+Menus on Restaurants.id = Menus.restaurant_id
+GROUP BY name;
 
-  --MenuItems
-SELECT * from MenuItems;
--- End of Read --
+-- -- SELECT all the menus, with the total cost of all the menu items summed, and have the list in descending order (most expensive first). Hint: you will need to use the SUM, GROUP BY and ORDER BY keywords
 
--- DELETE and Filter 
-DELETE from MenuItems WHERE name="Plain Pasta";
--- Read Back entries in MenuItems
-SELECT * FROM MenuItems;
+SELECT title, SUM(price) FROM
+Menus JOIN MenuItems ON
+Menus.id = MenuItems.menu_id
+GROUP BY title ORDER BY price DESC;
+-- End of Joins --
